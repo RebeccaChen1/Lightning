83124a2d6c61f98f46b1883942fc043a835e312f
unknown <rechen@697-334-Frodo.sf.k12.internal>
2014-09-02 11:06:28 -0700

    Lightning program

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,46 @@
+int startX=150;
+int startY=60;
+int endX=150;
+int endY=0;
+
 void setup()
 {
   size(300,300);
+  background(0);
+  strokeWeight(5);
 }
 void draw()
 {
-
+  background(0);
+  clouds();
+  while(endY<300)
+  {
+    stroke((int)(random(255)),(int)(random(255)),(int)(random(255)));
+    endX = startX+(int)(random(-9,9));
+    endY = startY+(int)(random(0,9));
+    line(startX,startY,endX,endY);
+    startX = endX;
+    startY = endY;
+  }
 }
-void mousePressed()
+void mouseMoved()
 {
+  startX=150;
+  startY=60;
+  endX=150;
+  endY=0;
+  background(255,255,255);
+}
+void clouds()
+{
+ noStroke();
+ fill(70,70,70);
+ ellipse(30,20,80,80);
+ ellipse(80,20,80,70);
+ ellipse(140,20,80,80);
+ ellipse(190,20,80,70);
+ ellipse(210,20,80,90);
+ ellipse(250,20,80,90);
+ ellipse(280,20,80,60);
 
 }
-
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,21 +14,52 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+int startX=150;
+int startY=60;
+int endX=150;
+int endY=0;
+
 public void setup()
 {
   size(300,300);
-  noLoop();
+  background(0);
+  strokeWeight(5);
 }
 public void draw()
 {
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+  background(0);
+  clouds();
+  while(endY<300)
+  {
+    stroke((int)(random(255)),(int)(random(255)),(int)(random(255)));
+    endX = startX+(int)(random(-9,9));
+    endY = startY+(int)(random(0,9));
+    line(startX,startY,endX,endY);
+    startX = endX;
+    startY = endY;
+  }
 }
-public void mousePressed()
+public void mouseMoved()
 {
-  redraw();
+  startX=150;
+  startY=60;
+  endX=150;
+  endY=0;
+  background(255,255,255);
 }
+public void clouds()
+{
+ noStroke();
+ fill(70,70,70);
+ ellipse(30,20,80,80);
+ ellipse(80,20,80,70);
+ ellipse(140,20,80,80);
+ ellipse(190,20,80,70);
+ ellipse(210,20,80,90);
+ ellipse(250,20,80,90);
+ ellipse(280,20,80,60);
 
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Lightning" };
     if (passedArgs != null) {
